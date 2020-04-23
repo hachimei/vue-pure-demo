70f87ad3fc3386871a30a8b779f109175558d231
hugt <hugt@mapout.net>
2020-04-23 09:59:32 +0800

    add axios and vue-axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import 'view-design/dist/styles/iview.css'
 import App from './App.vue'
 import ViewUI from 'view-design'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 // import './index.less'
 import '@/assets/mapbox/mapbox-gl.css'
 
 Vue.use(ViewUI)
+Vue.use(VueAxios, axios)
 
 Vue.prototype.$Message.config({
   top: 50,
